fa03866eb75033b6a5d498873062e0a017db50b0
SammySheu <samsheu1997@gmail.com>
2022-07-20 15:22:05 +0800

    Finishi 05 exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,15 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+  let answer = 0 ;
+  if(typeof(num1) == "number" && typeof(num2) == "number" && num1>0 && num2>0){
+    if(num1>num2)
+      [num1, num2] = [num2, num1];
+    for(let i = num1; i<=num2 ; i++)
+      answer += i ;
+  }
+  else
+    return "ERROR";
+  return answer;
 };
-
+// console.log( !isNaN("90") ) ;
 // Do not edit below this line
 module.exports = sumAll;
