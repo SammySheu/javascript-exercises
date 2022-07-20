f2d16eb5a140b3931e161b4793e31eb523be1cce
SammySheu <samsheu1997@gmail.com>
2022-07-20 14:50:01 +0800

    Finish 02 exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+  if(num<0) return "ERROR";
+  let response = "";
+  for(let i=0 ; i<num ; i++){
+    response += string;
+  }
+  // console.log(response);
+  // alert(response);
+  return response;
 };
 
 // Do not edit below this line
